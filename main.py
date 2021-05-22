4b75d8442624e4eba50020ba23396fce78ba11a1
Meysam Parvizi <meysam.parvizi.ir@gmail.com>
2021-05-22 23:53:12 +0430

    Solve running of process after closing window

--- main.py
+++ main.py
@@ -7,5 +7,10 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 img = cv2.imread(base_dir + "/watch.jpg", cv2.IMREAD_GRAYSCALE)
 cv2.imshow('image', img)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+# https://www.programmersought.com/article/17875689841/
+# blocking the program running after the OpenCV window is manually closed
+while cv2.waitKey(100) != 27:# loop if not get ESC
+  if cv2.getWindowProperty('image',cv2.WND_PROP_VISIBLE) <= 0:
+    break
+cv2.destroyWindow('image')
